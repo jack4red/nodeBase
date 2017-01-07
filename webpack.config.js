9cecc7d6733066550fecf1f764be5c5802c172a2
topPC_zk <1120429511@qq.com>
2017-01-07 12:52:25 +0800

    support es6

--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,17 @@ module.exports = {
 		new webpack.DefinePlugin({
 			__VERSION__: JSON.stringify(version)
 		})
-	]
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
 };
